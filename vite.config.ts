a4aa8d5fac5375a6112da6a87924fb37f015c7ec
lxycreate <1406976140@qq.com>
2023-02-25 13:38:14 +0800

    feat: 增加vite启动

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": "/src",
+            "@views": "/src/views",
+            "@router": "/src/router",
+            "@layout": "/src/layout",
+            "@styles": "/src/styles",
+        },
+    },
+    server: { open: true },
+    css: {
+        preprocessorOptions: {
+            less: {
+                javascriptEnabled: true,
+                additionalData: `@import "${resolve(
+                    __dirname,
+                    "src/styles/mixins.less"
+                )}";`,
+            },
+        },
+    },
+});
